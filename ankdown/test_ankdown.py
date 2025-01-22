82dc8588eb208ee99e774d4a3e6e2651da43ebed
qianweishuo <qianweishuo@bytedance.com>
2025-01-22 15:02:38 +0800

    MOD: upgrade req version to run test

--- ankdown/test_ankdown.py
+++ ankdown/test_ankdown.py
@@ -8,10 +8,16 @@ doctest非常脑残；无法正确理解python string中的转义
 Authors: qianweishuo<qianweishuo@bytedance.com>
 Date:    2020/3/26 11:51 下午
 """
-from ankdown.ankdown import textcolor2color
+from ankdown.ankdown import compile_field, textcolor2color
 
 
 def test_textcolor2color():
     latex_in = r"a \textcolor[rgb]{0.82,0.01,0.11}{f(x)} b"
     latex_out = textcolor2color(latex_in)
     assert latex_out == r"a \color{#D1021C}{f(x)} b"
+
+
+def test_surrounded_bold():
+    mkd_in = r"a *bold1* b, c*bold2*d"
+    mkd_out = compile_field(mkd_in, is_markdown=True)
+    assert mkd_out == r"a <em>bold1</em> b, c<em>bold2</em>d"
